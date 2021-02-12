e6385988a5d492c14f2aa30a1467f1d259df520d
smv5047 <smv5047@gmail.com>
2021-02-12 16:40:20 -0500

    updated pages

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Nav() {
+    return (<nav>
+        <Link href="/products">Products</Link>
+        <Link href="/sell">Sell</Link>
+        <Link href="/orders">Order</Link>
+        <Link href="/account">Account</Link>
+
+    </nav>)
+}
---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import Header from './Header'
+
+export default function Page({children}) {
+    return (<div>
+        <Header>
+            <h2>I am the page component</h2>
+            {children}
+        </Header>
+    </div>)
+}
+
+Page.propTypes = {
+    children: PropTypes.any,
+}
